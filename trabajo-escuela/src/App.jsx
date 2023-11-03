1f731aefce0e3e12d8d982a8ef73fe33bc96d75a
Leandr0Dev <leandrosantillan4@gmail.com>
2023-11-03 18:48:07 -0300

    React

---
+++ trabajo-escuela/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  //HTML
+  return (
+    <>
+      <div>
+        <form action="">
+        <h1>Iniciar Usuario</h1>
+        <input type="text" placeholder='Nombre' />
+        <input placeholder='Contraseña' type='password'></input> 
+        <button>Entrar</button>
+        </form>
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
