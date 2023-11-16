52b318125a146f5b4b6867610916adf6987f3a5c
Alexis0527 <velezalexis66@gmail.com>
2023-11-16 19:45:48 -0300

    arreglados put y post + detalles

--- API/routers/alumnos.js
+++ API/routers/alumnos.js
@@ -1,10 +1,8 @@
 import express from "express";
 import { db } from "../db.js";
-import cors from "cors";
 
 export const alumnoRouter = express
     .Router()
-    .use(cors())
     // [GET] Obtener Todos los Alumnos
     .get("/", async (req, res) => {
         const [rows, fields] = await db.execute("SELECT * FROM alumno");
@@ -28,11 +26,11 @@ export const alumnoRouter = express
     .post("/", async (req, res) => {
       const alumno = req.body.alumno;
       const [rows] = await db.execute(
-        "INSERT INTO alumno (idalumno,nombre,apellido,dni,fechaNacimiento,direccion,nomCompletoTutor,direccionTutor)VALUES(:idalumno ,:nombre,:apellido,:dni>,:fechaNacimiento,:direccion,:nomCompletoTutor,:numeroTutor,:direccionTutor)",
+        "INSERT INTO alumno (nombre,apellido,dni,fechaNacimiento,direccion,nomCompletoTutor,numeroTutor,direccionTutor)VALUES(:nombre,:apellido,:dni,:fechaNacimiento,:direccion,:nomCompletoTutor,:numeroTutor,:direccionTutor)",
         { nombre: alumno.nombre,
           apellido: alumno.apellido,
           dni: alumno.dni,
-          fechaNaciminto: alumno.fechaNacimiento,
+          fechaNacimiento: alumno.fechaNacimiento,
           direccion: alumno.direccion,
           nomCompletoTutor: alumno.nomCompletoTutor,
           numeroTutor: alumno.numeroTutor,
@@ -41,12 +39,20 @@ export const alumnoRouter = express
       res.status(201).send({ ...alumno, id: rows.insertId });
   })
     // [PUT] Modificar Informacion Por ID
-    .put("/alumno/:id", async (req, res) => {
+    .put("/:id", async (req, res) => {
         const id = req.params.id;
         const alumno = req.body.alumno;
         await db.execute(
           "UPDATE alumno SET nombre=:nombre, apellido=:apellido, dni=:dni, fechaNacimiento=:fechaNacimiento, direccion=:direccion, nomCompletoTutor=:nomCompletoTutor, numeroTutor=:numeroTutor, direccionTutor=:direccionTutor WHERE idalumno=:id",
-          { id, nombre: alumno.nombre, apellido: alumno.apellido, dni: alumno.dni, fechaNacimiento: alumno.fechaNacimiento, direccion: alumno.direccion, nomCompletoTutor: alumno.nomCompletoTutor, numeroTutor: alumno.numeroTutor, direccionTutor: alumno.direccionTutor }
+          { id, 
+            nombre: alumno.nombre,
+            apellido: alumno.apellido,
+            dni: alumno.dni,
+            fechaNacimiento: alumno.fechaNacimiento,
+            direccion: alumno.direccion,
+            nomCompletoTutor: alumno.nomCompletoTutor,
+            numeroTutor: alumno.numeroTutor,
+            direccionTutor: alumno.direccionTutor }
         );
         res.send("ok");
     });
